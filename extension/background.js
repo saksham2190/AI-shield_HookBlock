c9cc3eb5dbced74cc1934664a7d85289b52262c2
saksham9140 <sakshamyadav9140@gmail.com>
2026-07-13 15:14:00 +0530

    feat: connect extension to deployed Render backend

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api/analyze";
+const API_URL = "https://ai-shield-hookblock.onrender.com";
 
 // Extension Installed
 chrome.runtime.onInstalled.addListener(() => {
--- extension/utils/api.js
+++ extension/utils/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api/analyze";
+const API_URL = "https://ai-shield-hookblock.onrender.com";
 
 async function analyzeWebsite(url) {
 
